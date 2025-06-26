c18187e3fe97e6ff750a25f0b747c653729b528c
Ed <67418738+FreeHK-Lunity@users.noreply.github.com>
2025-06-26 15:36:51 +0800

    Util

---
+++ README.md
@@ -0,0 +1,11 @@
+# Project-HONGSHING
+
+Welcome to the Project-HONGSHING OMSI2 map contribution guide! This guide will walk you through the steps of contributing to the map.
+
+To learn how to use Git to clone and push, click [here](https://github.com/FreeHK-Lunity/KTKW-CPRKC/blob/main/Util/how-to-use-git.md)
+
+To learn how to push your changes, click [here](https://github.com/FreeHK-Lunity/KTKW-CPRKC/blob/main/Util/how-to-PR.md)
+
+## Note
+
+Remember to copy your whole "maps\KTKW-CPRKC" folder to the "KTKW-CPRKC" folder inside your main OMSI 2 folder whenever you finish editing your map.
---
+++ Util/ObjSplAIHum_Automation.py
@@ -0,0 +1,87 @@
+import re
+
+pattern = re.compile(r"vehicles\\\\")
+import glob
+import os
+
+path_of_map = '../'
+# Find all files with .txt extension in a directory
+files = [file for file in os.listdir(path_of_map) if file.endswith(".map")]
+#print(files)
+line_before_spline = 9999
+line_before_object = 9999
+list_of_map_tiles = []
+list_of_splines_used = []
+list_of_objects_used = []
+# Loop through each file and read its contents
+for file in files:
+    data = open(path_of_map+file, encoding='utf-16').read()
+    for line in data.splitlines():
+        line_before_spline += 1
+        line_before_object += 1
+        if line == '[spline]':
+            line_before_spline = 1
+        if line_before_spline == 3:
+            x = line
+            list_of_splines_used.append(x)
+        if line == '[object]':
+            line_before_object = 1
+        if line_before_object == 3:
+            x = line
+            list_of_objects_used.append(x)
+        
+
+s = set()
+
+# Define a new list to store the non-repeated strings
+list_of_splines_used_2 = []
+
+# Loop through each string in the list
+for x in list_of_splines_used:
+    # Check if the string is not in the set
+    if x not in s:
+        # Add the string to the set and the new list
+        s.add(x)
+        list_of_splines_used_2.append(x)
+
+s2 = set()
+
+# Define a new list to store the non-repeated strings
+list_of_objects_used_2 = []
+
+# Loop through each string in the list
+for x in list_of_objects_used:
+    # Check if the string is not in the set
+    if x not in s2:
+        # Add the string to the set and the new list
+        s.add(x)
+        list_of_objects_used_2.append(x)
+
+
+write_to_spline_txt = open('./Splines.txt', 'w', encoding='utf-8')
+for x in list_of_splines_used_2:
+    write_to_spline_txt.write(x+'\n')
+
+write_to_AICarandBus_txt = open('./AI_Vehicles.txt', 'w', encoding='utf-8')
+open_ai_list = open('maps/project-HONGSHING/ailists.cfg', 'r', encoding='utf-8')
+open_ai_list_2 = open_ai_list.readlines()
+#print(open_ai_list_2)
+
+for line in open_ai_list_2:
+    if 'vehicles' in line:
+        write_to_AICarandBus_txt.write(line)
+
+write_to_object_txt = open('./Objects.txt', 'w', encoding='utf-8')
+for x in list_of_objects_used_2:
+    write_to_object_txt.write(x+'\n')
+
+humans_text = open(f'{path_of_map}humans.txt', 'r', encoding='utf-8')
+humans_text_2 = humans_text.readlines()
+drivers_text = open(f'{path_of_map}drivers.txt', 'r', encoding='utf-8')
+drivers_text_2 = drivers_text.readlines()
+write_to_humans_txt = open('./Humans.txt', 'w', encoding='utf-8')
+for i in humans_text_2 + drivers_text_2:
+    if '\n' not in i:
+        i += '\n'
+    write_to_humans_txt.write(i)
+
